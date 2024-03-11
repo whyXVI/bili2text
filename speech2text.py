e7ae0ae8f2341014289d9b7be88722bc64e3cc65
whyXVI <139840508+whyXVI@users.noreply.github.com>
2024-03-11 12:43:28 +0800

    Update speech2text.py

--- speech2text.py
+++ speech2text.py
@@ -2,13 +2,18 @@ import whisper
 import os
 from tqdm import tqdm
 whisper_model = None
-
+transcribe_kwargs: dict[str, Optional[str]] = {
+    'model': 'tiny',
+    'prompt': '以下是普通话的句子。'
+    'language': None,
+    # 'language': 'zh',
+}
 def load_whisper(model="tiny"):
     global whisper_model
     whisper_model = whisper.load_model(model)
     print("Whisper模型："+model)
 
-def run_analysis(filename, model="tiny", prompt="以下是普通话的句子。"):
+def run_analysis(filename, **transcribe_kwargs):
     global whisper_model
     print("正在加载Whisper模型...")
     # 读取列表中的音频文件
@@ -22,7 +27,11 @@ def run_analysis(filename, model="tiny", prompt="以下是普通话的句子。"
     for fn in audio_list:
         print(f"正在转换第{i}/{len(audio_list)}个音频...")
         # 识别音频
-        result = whisper_model.transcribe(f"audio/slice/{filename}/{fn}", initial_prompt=prompt)
+        transcribe_kwargs["initial_prompt"] = transcribe_kwargs.get("prompt", "以下是普通话的句子。")
+        transcribe_kwargs.pop("model", None)
+        transcribe_kwargs.pop("prompt", None)
+        
+        result = whisper_model.transcribe(f"audio/slice/{filename}/{fn}", **transcribe_kwargs)
         print("".join([i["text"] for i in result["segments"] if i is not None]))
 
         with open(f"outputs/{filename}.txt", "a", encoding="utf-8") as f:
